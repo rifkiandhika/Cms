dd178f9b6b128fa68523da344c3f486efde02feb
rifkiandhika <rifkiandhika377@gmail.com>
2026-02-23 11:19:39 +0700

    update produks to primary

--- database/migrations/2026_02_10_015734_create_produks_table.php
+++ database/migrations/2026_02_10_015734_create_produks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('produks', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('kode_produk')->unique();
             $table->string('nie')->comment('Nomor Izin Edar');
             $table->string('nama_produk');
